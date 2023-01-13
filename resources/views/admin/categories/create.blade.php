7fc31fcea082941c58dfcb87593ce61afd420a45
theprinceofcodes <prince@underthehammer.com>
2023-01-13 22:13:30 +0100

    all validation errors displayed on the view

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -15,18 +15,28 @@
                 <form enctype="multipart/form-data" method="POST" action="{{ route('admin.categories.store')}}">
                     @csrf
                     <div class="sm:col-span-6">
+                    @error('name')
+                        <div class="text-sm text-red-500">{{ $message }}</div>
+                    @enderror
                     <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
                     <div class="mt-1">
                         <input type="text" id="name" name="name" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
                     </div>
                     <div class="sm:col-span-6">
+                    @error('image')
+                        <div class="text-sm text-red-500">{{ $message }}</div>
+                    @enderror
                     <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
+                    
                     <div class="mt-1">
                         <input type="file" id="image" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
                     </div>
                     <div class="sm:col-span-6 pt-5">
+                    @error('description')
+                        <div class="text-sm text-red-500">{{ $message }}</div>
+                    @enderror
                     <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
                     <div class="mt-1">
                         <textarea id="description" name="description" rows="3"  class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -15,24 +15,36 @@
             <form enctype="multipart/form-data" method="POST" action="{{ route('admin.reservations.store')}}">
                     @csrf
                     <div class="sm:col-span-6">
+                    @error('first_name')
+                        <div class="text-sm text-red-500">{{ $message }}</div>
+                    @enderror
                     <label for="first_name" class="block text-sm font-medium text-gray-700"> First Name </label>
                     <div class="mt-1">
                         <input type="text" id="first_name" name="first_name" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
                     </div>
                     <div class="sm:col-span-6">
-                    <label for="last_name" class="block text-sm font-medium text-gray-700"> First Name </label>
+                    @error('last_name')
+                        <div class="text-sm text-red-500">{{ $message }}</div>
+                    @enderror
+                    <label for="last_name" class="block text-sm font-medium text-gray-700"> Last Name </label>
                     <div class="mt-1">
                         <input type="text" id="last_name" name="last_name" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
                     </div>
                     <div class="sm:col-span-6">
+                    @error('email')
+                        <div class="text-sm text-red-500">{{ $message }}</div>
+                    @enderror
                     <label for="email" class="block text-sm font-medium text-gray-700"> Email</label>
                     <div class="mt-1">
                         <input type="email" id="email" name="email" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
                     </div>
                     <div class="sm:col-span-6">
+                    @error('tel_number')
+                        <div class="text-sm text-red-500">{{ $message }}</div>
+                    @enderror
                     <label for="tel_number" class="block text-sm font-medium text-gray-700"> tel number </label>
                     <div class="mt-1">
                         <input type="number" id="tel_number" name="tel_number" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
@@ -52,12 +64,18 @@
                     </div>
         
                     <div class="sm:col-span-6 pt-5">
+                    @error('res_date')
+                        <div class="text-sm text-red-500">{{ $message }}</div>
+                    @enderror
                     <label for="res_date" class="block text-sm font-medium text-gray-700">Reservation Date</label>
                     <div class="mt-1">
                         <input type="datetime-local" id="res_date" name="res_date" rows="3"  class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
                     </div>
                     </div>
                     <div class="sm:col-span-6">
+                    @error('guest_number')
+                        <div class="text-sm text-red-500">{{ $message }}</div>
+                    @enderror
                     <label for="guest_number" class="block text-sm font-medium text-gray-700"> Guest Number </label>
                     <div class="mt-1">
                         <input type="number" id="guest_number" name="guest_number" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
