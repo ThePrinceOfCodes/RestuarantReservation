6c09680e7f83ede25d84926e52b121884befdea7
theprinceofcodes <prince@underthehammer.com>
2023-01-13 14:38:05 +0100

    Table component for admin dashboard cred completed

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\TableLocation;
+use App\Enums\TableStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +17,11 @@ class Table extends Model
         'status',
         'location',
     ];
+
+    protected $casts = [
+        'status' => TableStatus::class,
+        'location' => TableLocation::class
+    ];
+
+    
 }
--- resources/views/admin/tables/edit.blade.php
+++ resources/views/admin/tables/edit.blade.php
@@ -12,18 +12,19 @@
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
             <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                <form  method="POST" action="{{ route('admin.tables.store')}}">
+                <form  method="POST" action="{{ route('admin.tables.update', $table->id)}}">
                     @csrf
+                    @method('PUT')
                     <div class="sm:col-span-6">
-                    <label for="name" class="block text-sm font-medium text-gray-700"> Name </label> 
+                    <label for="name" class="block text-sm font-medium text-gray-700" > Name </label>
                     <div class="mt-1">
-                        <input type="text" id="name" name="name" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                        <input type="text" id="name" value="{{ $table->name }}" name="name" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
                     </div>
                     <div class="sm:col-span-6">
-                    <label for="guest_number" class="block text-sm font-medium text-gray-700"> Guest Number</label>
+                    <label for="name" class="block text-sm font-medium text-gray-700" > Guest Number</label>
                     <div class="mt-1">
-                        <input type="number" id="guest_number" name="guest_number" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                        <input type="text" id="guest_number" value="{{ $table->guest_number }}" name="guest_number" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                     </div>
                     </div>
                     <div class="sm:col-span-6 pt-5">
@@ -31,7 +32,7 @@
                     <div class="mt-1">
                         <select name="status" class="form block w-full mt-1">
                             @foreach (App\Enums\TableStatus::cases() as $status)
-                                <option value="{{ $status->value }}">
+                                <option value="{{ $status->value }}" @selected($status->name)>
                                     {{ $status->name }}
                                 </option>
                             @endforeach
@@ -43,15 +44,17 @@
                     <div class="mt-1">
                         <select name="location" class="form block w-full mt-1 ">
                             @foreach (App\Enums\TableLocation::cases() as $location)
-                                <option value="{{ $location->value }}">
+                                <option value="{{ $location->value }}" @selected($location->name)>
                                     {{ $location->name }}
                                 </option>
                             @endforeach
                         </select>
                     </div>
                     </div>
+                   
+                   
                     <div class="mt-6">
-                        <button type="submit" class="px-4 py-2 bg-indigo-300 hover:bg-indigo-500 rounded-lg text-gray hover:text-white">Submit</button>
+                        <button type="submit" class="px-4 py-2 bg-indigo-300 hover:bg-indigo-500 rounded-lg text-gray hover:text-white">Update</button>
                     </div>
                 </form>
             </div>
